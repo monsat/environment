dddf96e32ca774d7b356db0b401fc834af117034
mon_sat <tanaka.kohji+github@gmail.com>
2012-05-01 20:40:04 +0900

    updated tests

--- Test/Case/Lib/EnvironmentTest.php
+++ Test/Case/Lib/EnvironmentTest.php
@@ -4,16 +4,15 @@ App::import('Lib', 'Environment.Environment');
 
 class EnvironmentTest extends CakeTestCase {
 
-	protected $_settings;
+	protected $_envs;
 	protected $_constant;
 
 	public function setUp() {
-		$this->_settings = Environment::$settings;
+		$this->_envs = Environment::$envs;
 		Environment::initialize(array(
-			'Develop' => array(
-				'www.dev.example.jp',
-				'monsat.dev.example.jp',
-				'dev.example.jp',
+			'Production' => array(
+				'www.example.com',
+				'example.com',
 			),
 			'Staging' => array(
 				'www.example.jp',
@@ -21,16 +20,18 @@ class EnvironmentTest extends CakeTestCase {
 				'monsat.example.jp',
 				'example.jp',
 			),
-			'Production' => array(
-				'www.example.com',
-				'example.com',
+			'Develop' => array(
+				'www.dev.example.jp',
+				'monsat.dev.example.jp',
+				'dev.example.jp',
 			),
+			'Empty' => array(),
 		));
 		$this->_constant = Environment::$constant;
 	}
 
 	public function tearDown() {
-		Environment::$settings = $this->_settings;
+		Environment::$envs = $this->_envs;
 		Environment::$constant = $this->_constant;
 	}
 
@@ -40,11 +41,11 @@ class EnvironmentTest extends CakeTestCase {
 	}
 
 	public function testIs() {
-		// Develop
-		$this->assertTrue(Environment::is('Develop', '/virtual/www.dev.example.jp'));
-		$this->assertTrue(Environment::is('Develop', '/virtual/monsat.dev.example.jp'));
-		$this->assertTrue(Environment::is('Develop', '/virtual/dev.example.jp'));
-		$this->assertFalse(Environment::is('Develop', '/virtual/monsat.example.jp'));
+		// Production
+		$this->assertTrue(Environment::is('Production', '/virtual/www.example.com'));
+		$this->assertTrue(Environment::is('Production', '/virtual/example.com'));
+		$this->assertFalse(Environment::is('Production', '/virtual/monsat.example.com'));
+		$this->assertFalse(Environment::is('Production', '/virtual/www.example.jp'));
 		// Staging
 		$this->assertTrue(Environment::is('Staging', '/virtual/www.example.jp'));
 		$this->assertTrue(Environment::is('Staging', '/virtual/www.example.jp'));
@@ -53,27 +54,18 @@ class EnvironmentTest extends CakeTestCase {
 		$this->assertTrue(Environment::is('Staging', '/virtual/example.jp'));
 		$this->assertFalse(Environment::is('Staging', '/virtual/dev.monsat.example.jp'));
 		$this->assertFalse(Environment::is('Staging', '/virtual/www.example.com'));
-		// Production
-		$this->assertTrue(Environment::is('Production', '/virtual/www.example.com'));
-		$this->assertTrue(Environment::is('Production', '/virtual/example.com'));
-		$this->assertFalse(Environment::is('Production', '/virtual/monsat.example.com'));
-		$this->assertFalse(Environment::is('Production', '/virtual/www.example.jp'));
+		// Develop
+		$this->assertTrue(Environment::is('Develop', '/virtual/www.dev.example.jp'));
+		$this->assertTrue(Environment::is('Develop', '/virtual/monsat.dev.example.jp'));
+		$this->assertTrue(Environment::is('Develop', '/virtual/dev.example.jp'));
+		$this->assertFalse(Environment::is('Develop', '/virtual/monsat.example.jp'));
 	}
 
-	public function testGetEnvName() {
-		$this->assertSame(Environment::getEnvName('/virtual/www.example.com'), 'www');
-		$this->assertSame(Environment::getEnvName('/virtual/www.example.jp'), 'www');
-		$this->assertSame(Environment::getEnvName('/virtual/example.jp'), '');
-		$this->assertSame(Environment::getEnvName('/virtual/monsat.example.jp'), 'monsat');
-		$this->assertSame(Environment::getEnvName('/virtual/monsat.dev.example.jp'), 'monsat');
-	}
-
-	public function testSettings() {
-		$settings = array(
-			'Develop' => array(
-				'www.dev.example.jp',
-				'monsat.dev.example.jp',
-				'dev.example.jp',
+	public function testEnvs() {
+		$envs = array(
+			'Production' => array(
+				'www.example.com',
+				'example.com',
 			),
 			'Staging' => array(
 				'www.example.jp',
@@ -81,14 +73,15 @@ class EnvironmentTest extends CakeTestCase {
 				'monsat.example.jp',
 				'example.jp',
 			),
-			'Production' => array(
-				'www.example.com',
-				'example.com',
+			'Develop' => array(
+				'www.dev.example.jp',
+				'monsat.dev.example.jp',
+				'dev.example.jp',
 			),
 		);
-		Environment::initialize($settings);
+		Environment::initialize($envs);
 
-		$this->assertEquals(Environment::$settings, $settings);
+		$this->assertEquals(Environment::$envs, $envs);
 
 		define('DEVELOP_CONSTANT_FOR_ENVIRONMENT', 'monsat.dev.example.jp');
 		Environment::$constant = 'DEVELOP_CONSTANT_FOR_ENVIRONMENT';
@@ -97,13 +90,14 @@ class EnvironmentTest extends CakeTestCase {
 	}
 
 	public function testIsCLIAndIsWeb() {
-		$backup = $_ENV;
+		$_env = $_ENV;
+		$_server = $_SERVER;
 
 		$_ENV['argc'] = 2;
 		$this->assertTrue(Environment::isCLI());
 		$this->assertFalse(Environment::isWeb());
 
-		$_ENV['argc'] = 1;
+		$_ENV['argc'] = $_SERVER['argc'] = 1;
 		$this->assertFalse(Environment::isCLI());
 		$this->assertTrue(Environment::isWeb());
 
@@ -111,7 +105,8 @@ class EnvironmentTest extends CakeTestCase {
 		$this->assertFalse(Environment::isCLI());
 		$this->assertTrue(Environment::isWeb());
 
-		$_ENV = $backup;
+		$_ENV = $_env;
+		$_SERVER = $_server;
 	}
 
 }
